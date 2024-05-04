2902f0b488521964404ceabaf876cce388eea92d
saikat2912 <saikat2270@gmail.com>
2024-05-04 12:51:58 +0530

    Initial commit

---
+++ src/main/java/com/example/bookmy/BookMyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookmy/BookMyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
